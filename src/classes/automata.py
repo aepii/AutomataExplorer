d6827f44911411b781c1a1fa364af4a773863487
aepii <simentalma@yahoo.com>
2024-02-28 21:24:15 -0600

    Automata implementation and tests.

--- src/classes/automata.py
+++ src/classes/automata.py
@@ -1,4 +1,32 @@
 class State:
-    def __init__(self, name):
+    def __init__(self, name, accepting):
         self.name = name
-        self.transitions = set()
+        self.accepting = accepting
+        self.transitions = {}
+
+    def add(self, letter, state):
+        self.transitions[letter] = state
+
+    def count_edges(self):
+        return len(self.transitions)
+
+
+class Automata:
+    def __init__(self, states, initial_state):
+        self.states = states
+        self.current_state = initial_state
+
+    def process_input(self, input_string):
+        for symbol in input_string:
+            if symbol in self.current_state.transitions:
+                past_state = self.current_state.name
+                self.current_state = self.current_state.transitions[symbol]
+                print(f"{past_state}: {symbol} --> {self.current_state.name}")
+            else:
+                print(f"No path found for {symbol} in state {self.current_state.name}.")
+                return
+
+        if self.current_state.accepting:
+            print(f"{input_string} is accepted.")
+        else:
+            print(f"{input_string} is not accepted.")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from classes.language import FormalLanguage
+from classes.automata import State, Automata
 
 
 def main():
+    print("Formal Language Test: ")
     my_language = FormalLanguage()
     my_language.add("c")
     my_language.add("b")
@@ -14,6 +16,16 @@ def main():
     print(my_language.is_a_string("abfc"))
     print(my_language.is_a_string("A"))
 
+    print("\nAutomata Test: ")
+    my_state_a = State("q1", False)
+    my_state_b = State("q2", True)
+
+    my_state_a.add("a", my_state_b)
+    my_state_a.add("b", my_state_a)
+    my_state_b.add("b", my_state_a)
+
+    my_automata = Automata([my_state_a, my_state_b], my_state_a)
+    my_automata.process_input("bba")
 
 if __name__ == "__main__":
     main()
